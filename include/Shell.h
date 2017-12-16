27d4b3810161dd21283f2c5df09e08e50fecdd29
jardelcarvalho <jardelfelipe.carvalho@gmail.com>
2017-12-16 19:16:13 +0000

    agora vai

--- include/Shell.h
+++ include/Shell.h
@@ -1,5 +1,14 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+void init();
+void load();
+void ls(char param[]);
+void mkdir(char param[]);
+void create(char param[]);
+void unlink(char param[]);
+void read(char param[]);
+void write(char str[], char param[]);
+void append(char str[], char param[]);
 
 #endif
--- src/Shell.c
+++ src/Shell.c
@@ -4,8 +4,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void debug(){
-    printf("Debug");
+void init(){
+
+}
+
+void load(){
+
+}
+
+void ls(char param[]){
+    
+}
+
+void mkdir(char param[]){
+
+}
+
+void create(char param[]){
+    
+}
+
+void unlink(char param[]){
+
+}
+
+void read(char param[]){
+
+}
+
+void write(char str[], char param[]){
+
+}
+
+void append(char str[], char param[]){
+
 }
 
 int main(){
@@ -18,39 +50,48 @@ int main(){
 
         if(!strcmp(cmd, "init")){
             //init
+            init();
         }else if(!strcmp(cmd, "load")){
             //load
+            load();
         }else if(!strcmp(cmd, "exit")){
             //exit
             return 0;
         }else if(!strcmp(cmd, "ls")){
             scanf("%s", param);
             //ls
-        }else if(!strcmp(cmd, "mkparam")){
+            ls(param);
+        }else if(!strcmp(cmd, "mkdir")){
             scanf("%s", param);
-            //mkparam
+            //mkdir
+            mkdir(param);
         }else if(!strcmp(cmd, "create")){
             scanf("%s", param);
             //create
+            create(param);
         }else if(!strcmp(cmd, "unlink")){
             scanf("%s", param);
             //unlink
+            unlink(param);
         }else if(!strcmp(cmd, "read")){
             scanf("%s", param);
             //read
+            read(param);
         }else if(!strcmp(cmd, "write")){
             scanf("%s", str);
             scanf("%s", param);
             //write
+            write(str, param);
         }else if(!strcmp(cmd, "append")){
             scanf("%s", str);
             scanf("%s", param);
             //append
+            append(str, param);
         }else{
             printf("msg: comando inválido\n");
         }
 
-        printf("%s %s %s", cmd, str, param);
+        //printf("%s %s %s", cmd, str, param);
 
     }
 
